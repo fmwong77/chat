abed686a5784844095385e295fb16e6dffa08ffb
fmwong77 <fongmeiw@gmail.com>
2020-04-05 11:54:50 -0500

    create chat layout

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="container">
+			<aside id="sidebar">Users</aside>
+			<section id="main">
+				<section id="messages-list">Messages list</section>
+				<section id="new-message">New message</section>
+			</section>
+		</div>
+	);
 }
 
 export default App;
